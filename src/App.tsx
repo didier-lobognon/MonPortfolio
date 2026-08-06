5c285ce4f0f005cd51f91c81d1f2efda1db5d8b6
Didier Lobognon <didier.dogo@epitech.eu>
2026-08-06 17:27:04 +0000

    UPDATE : mise a jour de mon portfolio avec des composant react bit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { LoadingScreen } from '@/components/layout/LoadingScreen'
 import { CustomCursor } from '@/components/layout/CustomCursor'
 import { ScrollProgress } from '@/components/layout/ScrollProgress'
 import { SmoothScroll } from '@/components/layout/SmoothScroll'
+import { WhatsAppFloat } from '@/components/layout/WhatsAppFloat'
 import { Hero } from '@/components/sections/Hero'
 import { About } from '@/components/sections/About'
 import { Skills } from '@/components/sections/Skills'
@@ -46,6 +47,7 @@ export default function App() {
           </main>
 
           <Footer />
+          <WhatsAppFloat />
         </SmoothScroll>
       )}
     </>
